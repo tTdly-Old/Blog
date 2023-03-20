ef966e02585aa2b194a8838104b0466596169f41
tutoudenglongyu <tutoudenglongyu@163.com>
2023-03-20 11:30:55 +0800

    更改写入方式

--- .github/scripts/wirte.sh
+++ .github/scripts/wirte.sh
@@ -1,15 +1,24 @@
-# $1 title $2 body $3 labels $4 number $5 time
+TITLE=$(echo $1 | jq .event.issue.title)
+NUMBER=$(echo $1 | jq .event.issue.number)
+BODY=$(echo $1 | jq .event.issue.body)
+BODY=$(echo ${BODY:1:-1})
+TIME=$(echo $1 | jq .event.issue.created_at)
+LABLES=$(echo $RESULT | jq '.event.issue.labels | map(.name)')
+HEADYML="---\ntitle: $TITLE\ndate: $TIME\ntags: $LABLES---\n"
+
 if [ ! -d "posts" ];then
     mkdir posts
 fi
 
-LABLES=(echo $3 | jq '. | map(.name)')
-HEADYML="---\ntitle: $1\ndate: $5\n---"
-echo -e $HEADYML > $4.1
-echo -e $2 >> $4.1
+echo -e $HEADYML > $NUMBER.1
+echo -e $BODY >> $NUMBER.1
+
 while read line
 do
-  echo -e $line >> posts/$4.md
-done < $4.1
+  echo -e $line >> posts/$NUMBER.md
+done < $NUMBER.1
+cat posts/$NUMBER.md > index.md
 
 rm $NUMBER.1
+
+cat posts/$NUMBER.md > index.md
